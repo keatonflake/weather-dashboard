829419118988a5af24e01657957d376d22013f4b
Keaton <az.keaton@gmail.com>
2021-12-05 21:55:51 -0700

    adding js to display current weather

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,44 +1,43 @@
+// var typedCityName = document.querySelector("#CITY");
+// var formClassEl = document.querySelector("#formClass");
+// var weatherResultsEl = document.querySelector("#weatherResults");
+// var weatherIconEl = document.querySelector("#weatherIcon");
+// var tempEl = document.querySelector("#temp");
 
-var typedCityName = document.querySelector("#CITY");
-var formClassEl = document.querySelector("#formClass");
-var weatherResultsEl = document.querySelector("#weatherResults");
-var weatherIconEl = document.querySelector("#weatherIcon");
-var tempEl = document.querySelector("#temp");
+// var formSubmitHandler = function(event) {
 
-var formSubmitHandler = function(event) {
-
-    event.preventDefault();
+//     event.preventDefault();
     
-    let = cityName = typedCityName.value.trim();
+//     let = cityName = typedCityName.value.trim();
 
-    if (cityName) {
-        getCityForcast(cityName)
-    }
-}
+//     if (cityName) {
+//         getCityForcast(cityName)
+//     }
+// }
 
-var getCityForcast = function(selectedCity) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + selectedCity + "&appid=fb0edd857bab8092d150bbc076d9150a";
-// fetch the data
-fetch(apiUrl)
-    .then(function(response) {
+// var getCityForcast = function(selectedCity) {
+//     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + selectedCity + "&appid=fb0edd857bab8092d150bbc076d9150a";
+// // fetch the data
+// fetch(apiUrl)
+//     .then(function(response) {
 
-    if (response.ok) {
-        response.json().then(function(data) {
-            console.log(data);
-            weatherResultsEl.textContent = (data.name);
-            tempEl.innerHTML = (data.main.temp)
+//     if (response.ok) {
+//         response.json().then(function(data) {
+//             console.log(data);
+//             weatherResultsEl.textContent = (data.name);
+//             tempEl.innerHTML = (data.main.temp)
             
 
-        let SAVEDdataScript = JSON.parse(localStorage.getItem(data));
-            if(SAVEDdataScript === null) {
-                SAVEDdataScript = [];
-            }
-            SAVEDdataScript.push(data)
-            localStorage.setItem('weather', JSON.stringify(SAVEDdataScript))
+//         let SAVEDdataScript = JSON.parse(localStorage.getItem(data));
+//             if(SAVEDdataScript === null) {
+//                 SAVEDdataScript = [];
+//             }
+//             SAVEDdataScript.push(data)
+//             localStorage.setItem('weather', JSON.stringify(SAVEDdataScript))
                 
-        })
-    }
-})
-};
+//         })
+//     }
+// })
+// };
 
-formClassEl.addEventListener("click", formSubmitHandler)
+// formClassEl.addEventListener("click", formSubmitHandler)
---
+++ assets/JS/test.js
@@ -0,0 +1,70 @@
+const typedCityName = document.querySelector("#CITY");
+const formClassEl = document.querySelector("#formClass");
+
+const timeEl = document.getElementById('time')
+const dateEl = document.getElementById('date')
+const currentWeatherItemsEl = document.getElementById('currentWeather-Items')
+const weatherForecastEl = document.getElementById('weatherForecast')
+const currenttempEl = document.getElementById('currentTemp')
+
+const TEMP_El = document.querySelector("#TEMP");
+const HUMIDITY_El = document.querySelector("#HUMIDITY");
+const WINDSPEED_El = document.querySelector("#WINDSPEED");
+const UVI_El = document.querySelector("#UVI");
+
+const dayoftheweekEl = document.getElementById("dayoftheweek")
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+// const API_Key = '&appid=fb0edd857bab8092d150bbc076d9150a'
+
+// var formSubmitHandler = function(event) {
+
+//     event.preventDefault();
+     
+//      let = cityName = typedCityName.value.trim();
+ 
+//      if (cityName) {
+//          getWeatherData(cityName)
+//      }
+//  }
+
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrsFormat = hour >= 13 ? hour %12: hour
+    const minutes = time.getMinutes();
+    const ampm = hour >=12 ? 'PM' : 'AM'
+
+    dayoftheweekEl.innerHTML = days[day]
+    timeEl.innerHTML = hoursIn12HrsFormat + ':' + minutes+ ' ' + `<span id="am-pm">${ampm}</span>`
+
+    dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
+}, 1000)
+
+function getWeatherData () {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + "seattle" + "&units=imperial&appid=fb0edd857bab8092d150bbc076d9150a";
+    fetch(apiUrl)
+        .then(response => response.json()).then(data => {
+            console.log(data)
+            let TEMPSTRING = JSON.stringify(data.main.temp)
+            let HUMIDITYSTRING = JSON.stringify(data.main.humidity)
+            let WINDSPEEDSTRING = JSON.stringify(data.wind.speed)
+            
+            
+            TEMP_El.innerHTML = TEMPSTRING
+            HUMIDITY_El.innerHTML = HUMIDITYSTRING
+            WINDSPEED_El.innerHTML = WINDSPEEDSTRING
+    })
+}
+getWeatherData()
+// temperature, the humidity, the wind speed, and the UV index
+// function showWeatherData(data) {
+
+// }
+
+// formClassEl.addEventListener("click", formSubmitHandler)
